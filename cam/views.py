5d70e054a806243a70f598b31ebdde58fb82c4cc
sampiecz <samuel.piecz@gmail.com>
2021-02-07 20:05:17 -0600

    Checking in.

--- cam/views.py
+++ cam/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse,StreamingHttpResponse
+import cv2
+import time
 
 # Create your views here.
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        ret,image = self.video.read()
+        ret,jpeg = cv2.imencode('.jpg',image)
+        return jpeg.tobytes()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(frame)
+        time.sleep(1)
+
 def index(request):
-    return render(request, 'cam/index.html')
+    # response = HttpResponse(gen(VideoCamera())
+    return StreamingHttpResponse(gen(VideoCamera()),content_type="image/jpeg")
