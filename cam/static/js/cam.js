b036b1810ddd73dc5b091943751fa64c8010169b
sampiecz <samuel.piecz@gmail.com>
2021-05-02 02:50:03 -0500

    Was able to console log an image that was fetched.

--- cam/static/js/cam.js
+++ cam/static/js/cam.js
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function Cam() {
-  
-  function getStream() {
-    return fetch('/cam/stream')
-      .then(
-        data => data
-      )
-  }
+
+  const [imageSrc, setSrc] = useState();
+
+	useEffect( () => {
+		fetch('http://purp.local:8000/cam/stream')
+			.then(response => {
+				response.blob().then(
+					(result) => { 
+						console.log(result);
+					},
+					(error) => { 
+						console.log(error);
+					}
+				)
+				//const reader = .body.getReader();
+				//return new ReadableStream({
+				//	start(controller) {
+				//		return pump();
+				//		function pump() {
+				//			return reader.read().then(({ done, value }) => {
+				//				// When no more data needs to be consumed, close the stream
+				//				if (done) {
+				//					controller.close();
+				//					return;
+				//				}
+				//				// Enqueue the next data chunk into our target stream
+				//				controller.enqueue(value);
+				//				return pump();
+				//			});
+				//		}
+				//	}
+				//})
+			})
+	}, [imageSrc]);
 
   function up() {
-    console.log(getStream());
   }
 
   function down() {
-  
   }
 
   function left() {
-  
   }
 
   function right() {
-  
   }
 
   return (
   <>
     <div>
       <h1>Z Cam</h1>
-      <img style="width: 100%; height:100%;" src={getStream()} />
+      {/*
+				<img style="width: 100%; height:100%;" src={imageSrc} />
+			*/}
     </div>
     <ul>
       <li>
