cb8ac7050b5abd56e7e5f332c47b6eac33f26b7f
sampiecz <samuel.piecz@gmail.com>
2021-08-11 14:11:22 -0500

    Changed port number from 8000 to 9000 in navigation.

--- static/js/cam/navigation.js
+++ static/js/cam/navigation.js
@@ -10,16 +10,16 @@ export default function Navigation() {
             <a className="nav-brand" href="{% url 'index' %}"><span>Z</span></a>
           </td>
           <td>
-            <a className="nav-link" href="http://red.local:8000/cam/">Red</a>
+            <a className="nav-link" href="http://red.local:9000/cam/">Red</a>
           </td>
           <td>
-            <a className="nav-link" href="http://purp.local:8000/cam/">Purp</a>
+            <a className="nav-link" href="http://purp.local:9000/cam/">Purp</a>
           </td>
           <td>
-            <a className="nav-link" href="http://toxic.local:8000/cam/">Toxic</a>
+            <a className="nav-link" href="http://toxic.local:9000/cam/">Toxic</a>
           </td>
           <td>
-            <a className="nav-link" href="http://rainbow.local:8000/cam/">Rainbow</a>
+            <a className="nav-link" href="http://rainbow.local:9000/cam/">Rainbow</a>
           </td>
         </tr>
       </thead>
